b3c199b0c830129cf8beee5782debd2174e91c0c
ercjns <eric@ercjns.com>
2017-03-23 12:20:20 -0700

    deamon startup script

---
+++ startapp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo 'Activating python virtualenv'
+. venv/bin/activate
+echo 'creating or updating the db'
+export FLASK_APP=wotdapp
+python -m flask db upgrade
+echo 'Starting the app server'
+/home/protected/venv/bin/gunicorn --log-file gunicorn.log --log-level info -p app.pid wotdapp:app
